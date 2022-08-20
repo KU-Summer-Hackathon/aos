5268ebb7b3b7356c3f8a574ea38983dda1d701aa
Yunjoo Son <thsduswn4141@naver.com>
2022-08-21 03:31:25 +0900

    [layout] MessageRoomActivity 관련 레이아웃 설정

---
+++ app/src/main/java/com/yjooooo/doreandroid/presentation/message_room/MessageRoomActivity.kt
@@ -0,0 +1,13 @@
+package com.yjooooo.doreandroid.presentation.message_room
+
+import android.os.Bundle
+import com.yjooooo.doreandroid.R
+import com.yjooooo.doreandroid.databinding.ActivityMessageRoomBinding
+import com.yjooooo.doreandroid.presentation.base.BaseActivity
+
+class MessageRoomActivity :
+    BaseActivity<ActivityMessageRoomBinding>(R.layout.activity_message_room) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
