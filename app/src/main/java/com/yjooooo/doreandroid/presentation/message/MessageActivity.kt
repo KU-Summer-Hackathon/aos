11b904aaeb862490718111061a666f9ee0b1c427
Yunjoo Son <thsduswn4141@naver.com>
2022-08-20 10:51:48 +0900

    [layout] MessageActivity 레이아웃 설정

---
+++ app/src/main/java/com/yjooooo/doreandroid/presentation/message/MessageActivity.kt
@@ -0,0 +1,13 @@
+package com.yjooooo.doreandroid.presentation.message
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yjooooo.doreandroid.R
+import com.yjooooo.doreandroid.databinding.ActivityMessageBinding
+import com.yjooooo.doreandroid.presentation.base.BaseActivity
+
+class MessageActivity : BaseActivity<ActivityMessageBinding>(R.layout.activity_message) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
